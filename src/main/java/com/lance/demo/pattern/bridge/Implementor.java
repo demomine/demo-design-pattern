34b3d7077d345260e7da247118da0d59035e4631
perdoanre <perdonare@163.com>
2017-10-22 17:02:26 +0800

    refresh

--- src/main/java/com/lance/demo/pattern/bridge/Implementor.java
+++
@@ -1,8 +0,0 @@
-package com.lance.demo.pattern.bridge;
-
-/**
- * Created by immerer on 2017/10/22.
- */
-public abstract class Implementor {
-    public abstract void operationImpl();
-}
--- src/main/java/com/lance/demo/pattern/driver/Driver.java
+++
@@ -1,8 +0,0 @@
-package com.lance.demo.pattern.driver;
-
-/**
- * Created by immerer on 2017/10/22.
- */
-public interface Driver {
-    public void execute(String sql);
-}
